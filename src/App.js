1808e9c1d4636c48dfffcf08f8a1d17b4bc0c310
vanessa <veifrah@gmail.com>
2018-02-14 20:27:22 -0500

    Added paths for all routes.

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ class App extends Component {
       // return a Route component that loads up respective Components based on URL path
       return (
          <div>
-             <Route exact path="/" component={CustomerHomeContainer} />
-             <Route exact path="/" component={LoginContainer} />
-             <Route exact path="/" component={ReservationsContainer} />
-             <Route exact path="/" component={RestaurantProfileContainer} />
-             <Route exact path="/" component={SettingsContainer} />
+             <Route path="/customer" component={CustomerHomeContainer} />
+             <Route path="/login" component={LoginContainer} />
+             <Route path="/reservations" component={ReservationsContainer} />
+             <Route path="/restaurantprofile" component={RestaurantProfileContainer} />
+             <Route path="/settings" component={SettingsContainer} />
          </div>
       );
   }
