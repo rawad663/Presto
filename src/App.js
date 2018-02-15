abf628cabc26c1833a1d8b93845ab4e233b56b3e
vanessa <veifrah@gmail.com>
2018-02-14 20:02:19 -0500

    Added main routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
+import CustomerHomeContainer from './containers/CustomerHomeContainer';
 import LoginContainer from './containers/LoginContainer';
+import ReservationsContainer from './containers/ReservationsContainer';
+import RestaurantProfileContainer from './containers/RestaurantProfileContainer';
+import SettingsContainer from './containers/SettingsContainer';
 import './App.css';
 
 class App extends Component {
@@ -10,7 +14,11 @@ class App extends Component {
       // return a Route component that loads up respective Components based on URL path
       return (
          <div>
+             <Route exact path="/" component={CustomerHomeContainer} />
              <Route exact path="/" component={LoginContainer} />
+             <Route exact path="/" component={ReservationsContainer} />
+             <Route exact path="/" component={RestaurantProfileContainer} />
+             <Route exact path="/" component={SettingsContainer} />
          </div>
       );
   }
