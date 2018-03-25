dd7f9d58c5ba306105bd2fbb891053ff70ae9b03
Rawad Karam <rawad.karam@mail.mcgill.ca>
2018-03-25 17:41:39 -0400

    reverted api call

--- src/containers/CustomerHomeContainer/index.js
+++ src/containers/CustomerHomeContainer/index.js
@@ -17,83 +17,11 @@ export default class CustomerHomeContainer extends Component{
 
     componentDidMount() {
         this.setState({
-            items: [
-                {
-                    user: {
-                        username: 'gjgjhg@hg.com',
-                        email:'gjgjhg@hg.com',
-                        first_name: '988 Greene av.',
-                        last_name: 'ytrytdfbcx'
-                    },
-                    description: 'French food for the rich.  Hungry? Nice dude',
-                    resto_name: 'French Cuisine',
-                    address: '5600 Waldorf',
-                    postal_code: 'K7L8E3',
-                    phoneNumber: '657657646453'
-                },
-                {
-                    user: {
-                        username: 'gjgjhg@hg.com',
-                        email:'gjgjhg@hg.com',
-                        first_name: '34 Lucerne',
-                        last_name: 'ytrytdfbcx'
-                    },
-                    description: 'Bar that serves beer with lasagna and meatballs served in cold turkey with beef jerky legs',
-                    resto_name: 'Pasta Bar',
-                    address: '1247 Beauregard',
-                    postal_code: 'H3B4J6',
-                    phoneNumber: '657657646453'
-                },
-                {
-                    user: {
-                        username: 'gjgjhg@hg.com',
-                        email:'gjgjhg@hg.com',
-                        first_name: 'kjhkjh',
-                        last_name: 'ytrytdfbcx'
-                    },
-                    description: 'Call this restaurant Gentle cuz its gentille to ur body, mind, and soul.  ' +
-                    'Serves no alcohol what soever.  Sunday is the day of the lord, but Saturaday is the day of the king.' +
-                    'Now the old king is dead, long live the queen. ',
-                    resto_name: 'Gentile',
-                    address: '9299 Park Ave',
-                    postal_code: 'H2N2A2',
-                    phoneNumber: '657657646453'},
-                {
-                    user: {
-                        username: 'gjgjhg@hg.com',
-                        email:'gjgjhg@hg.com',
-                        first_name: 'kjhkjh',
-                        last_name: 'ytrytdfbcx'
-                    },
-                    description: 'Went to mars and back, we brought back mercury.  Opened this restaurant with the profits.  If u want a ' +
-                    'once in a lifetime experience do not come here, go to Iron across the street instead.',
-                    resto_name: 'Mercury',
-                    address: '555 Gordon st.',
-                    postal_code: 'J7G0P0',
-                    phoneNumber: '657657646453'},
-                {
-                    user: {
-                        username: 'gjgjhg@hg.com',
-                        email:'gjgjhg@hg.com',
-                        first_name: 'kjhkjh',
-                        last_name: 'ytrytdfbcx'
-                    },
-                    description: 'Jumbatron once told megatron that if the world was worth saving, it was solely because of ' +
-                    'how good Amirs food tasted.  Megatron is said to have eaten 8 diesel engines and 3 trusk tires here.  ' +
-                    'lightning mcqueen fans eat for free',
-                    resto_name: 'Amir',
-                    address: '432 Bertrand st.',
-                    postal_code: 'J1JE5F6',
-                    phoneNumber: '657657646453'}
-            ]
-        });
-
-        // aGet(routes.restos).then(response => {
-        //     this.setState({
-        //         items: response.data
-        //     });
-        // })
-
+        aGet(routes.restos).then(response => {
+            this.setState({
+                items: response.data
+            });
+        })
     }
 
     handleLike = id => {
